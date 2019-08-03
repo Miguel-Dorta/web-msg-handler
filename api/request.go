5797c450cb53a8557156049ed4c3cf3fef434db4
Miguel-Dorta <contact@migueldorta.com>
2019-08-04 00:32:00 +0100

    Comment public API

--- api/request.go
+++ api/request.go
@@ -1,5 +1,7 @@
 package api
 
+// Request represents the content of the request that web-msg-handler will accept.
+// It must be in JSON.
 type Request struct {
 	Name      string `json:"name"`
 	Mail      string `json:"mail"`
